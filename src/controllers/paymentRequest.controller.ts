fe748e3c251a0e3c09d41c8a5e21f732557c30e1
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-07-01 15:12:53 +0100

    implement creation of payment request, get sent and received requests

--- src/controllers/paymentRequest.controller.ts
+++ src/controllers/paymentRequest.controller.ts
@@ -1,6 +1,12 @@
 import { AppDataSource } from "./../database/data-source";
 import { Request, Response } from "express";
 import { Account } from "../database/entities/Account";
+import { User } from "../database/entities/User";
+import {
+  Transaction,
+  TransactionStatus,
+  TransactionType,
+} from "../database/entities/Transaction";
 
 const searchUser = async (req: Request, res: Response): Promise<Response> => {
   const { account_number } = req.query;
@@ -22,4 +28,105 @@ const searchUser = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json({ message: "Account Number ", result });
 };
 
-export { searchUser };
+const createPaymentRequest = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { account_number, amount, description } = req.body;
+  const accountRepository = AppDataSource.getRepository(Account);
+  const userRepository = AppDataSource.getRepository(User);
+  const transactionRepository = AppDataSource.getRepository(Transaction);
+
+  // make sure sender is validated
+  const sender = await userRepository.findOneBy({ id: req.currentUser?.id });
+  const receiverAccount = await accountRepository.findOne({
+    where: { account_number: account_number?.toString() },
+  });
+  if (!sender || !receiverAccount) {
+    return res
+      .status(404)
+      .json({ message: "Sender or receiver account not found!" });
+  }
+
+  // find receiver
+  const receiver: User = (await userRepository.findOneBy({
+    id: receiverAccount.user?.id,
+  })) as User;
+
+  // create new request (transaction)
+  const newRequest = new Transaction();
+  newRequest.user = sender;
+  newRequest.amount = amount;
+  newRequest.description = description;
+  newRequest.sender = sender;
+  newRequest.receiver = receiver;
+  newRequest.status = TransactionStatus.REQUEST_PROCESSING;
+  newRequest.transactionType = TransactionType.REQUEST;
+
+  await transactionRepository.save(newRequest);
+
+  return res.status(200).json({ message: "Payment Request created!" });
+};
+
+const recievedPaymentRequests = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const transactionRepository = AppDataSource.getRepository(Transaction);
+  const paymentRequests = await transactionRepository.find({
+    where: {
+      transactionType: TransactionType.REQUEST,
+      receiver: { id: req.currentUser?.id },
+    },
+    relations: ["sender", "receiver"],
+    order: { createdAt: "DESC" },
+  });
+
+  const results = paymentRequests.map((transaction) => {
+    return {
+      id: transaction.id,
+      amount: transaction.amount,
+      description: transaction.description,
+      status: transaction.status,
+      createdAt: transaction.createdAt,
+      senderUsername: transaction.sender?.username || null,
+    };
+  });
+
+  return res.status(200).json({ message: "Payment Request created!", results });
+};
+
+const sentPaymentRequests = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const transactionRepository = AppDataSource.getRepository(Transaction);
+  const paymentRequests = await transactionRepository.find({
+    where: {
+      transactionType: TransactionType.REQUEST,
+      sender: { id: req.currentUser?.id },
+    },
+    relations: ["sender", "receiver"],
+    order: { createdAt: "DESC" },
+  });
+
+  const results = paymentRequests.map((transaction) => {
+    return {
+      id: transaction.id,
+      amount: transaction.amount,
+      description: transaction.description,
+      status: transaction.status,
+      createdAt: transaction.createdAt,
+      receiverUsername: transaction.receiver?.username || null,
+    };
+  });
+
+  return res.status(200).json({ message: "Payment Request created!", results });
+};
+
+export {
+  searchUser,
+  createPaymentRequest,
+  recievedPaymentRequests,
+  sentPaymentRequests,
+};
--- src/routes/paymentRequest.route.ts
+++ src/routes/paymentRequest.route.ts
@@ -1,7 +1,16 @@
+import { AuthMiddleware } from "./../middleware/auth.middleware";
 import express from "express";
-import { searchUser } from "../controllers/paymentRequest.controller";
+import {
+  searchUser,
+  createPaymentRequest,
+  recievedPaymentRequests,
+  sentPaymentRequests,
+} from "../controllers/paymentRequest.controller";
 
 const router = express.Router();
 router.get("/searchUser", searchUser);
+router.post("/new", AuthMiddleware.authenticate, createPaymentRequest);
+router.get("/received", AuthMiddleware.authenticate, recievedPaymentRequests);
+router.get("/sent", AuthMiddleware.authenticate, sentPaymentRequests);
 
 export default router;

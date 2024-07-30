897342a9380a80746a96a4c9709d46eaddf06e5f
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-31 00:11:50 +0100

    dynamically connect to db

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -11,11 +11,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 5432,
-  username: "isaacoduh",
-  password: "root",
-  database: "clasp-dev",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: false,
   logging: ["query"],
   entities: [User, Kyc, Account, Transaction, CreditCard, Notification],

bed0be50438001a51b475cacd6026dba401765cc
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-31 00:52:15 +0100

    fix this shit

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,6 +8,7 @@ import { Transaction } from "./entities/Transaction";
 import { User } from "./entities/User";
 
 dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -21,5 +22,5 @@ export const AppDataSource = new DataSource({
   entities: [User, Kyc, Account, Transaction, CreditCard, Notification],
   subscribers: [],
   migrations: ["src/database/migrations/*.ts"],
-  ssl: { rejectUnauthorized: true },
+  ssl: isProduction ? { rejectUnauthorized: true } : false,
 });
